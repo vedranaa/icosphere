e9af00b5124fc0680faf9243358c30e2d5d4f7fd
Vedrana Andersen Dahl <vedranaa@gmail.com>
2026-04-11 13:09:25 +0200

    preparing for version 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'icosphere',
-    version = '0.1.4',
+    version = '0.2.0',
     description = 'Creates a geodesic icosahedron given a subdivision frequency.',
     long_description = long_description,
     long_description_content_type = "text/markdown",
